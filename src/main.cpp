b80e81c3133331716e43fe6293bd5fa7acded8ea
kamranrasul <kamran.rasul@gmail.com>
2020-07-26 12:59:11 -0600

    reading status of 8 controls and sending to master

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,88 @@
-#include <Arduino.h>
+/*********
+  Rui Santos
+*********/
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include <esp_now.h>
+#include <WiFi.h>
+
+// RECEIVER'S MAC Address
+uint8_t broadcastAddress[] = {0x24, 0x62, 0xAB, 0xF9, 0x0E, 0x98};
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message
+{
+  int id; // must be unique for each sender board
+  uint8_t pinStatus[8];
+} struct_message;
+
+//Create a struct_message called myData
+struct_message myData;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  // Init Serial Monitor
+  Serial.begin(115200);
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+
+void loop()
+{
+  // Set values to send
+  myData.id = 1;
+  myData.pinStatus[0] = digitalRead(00);
+  myData.pinStatus[1] = digitalRead(02);
+  myData.pinStatus[2] = digitalRead(04);
+  myData.pinStatus[3] = digitalRead(12);
+  myData.pinStatus[4] = digitalRead(14);
+  myData.pinStatus[5] = digitalRead(15);
+  myData.pinStatus[6] = digitalRead(26);
+  myData.pinStatus[7] = digitalRead(27);
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+
+  if (result == ESP_OK)
+  {
+    Serial.println("Sent with success");
+  }
+  else
+  {
+    Serial.println("Error sending the data");
+  }
+  delay(10000);
 }
