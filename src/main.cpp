726bddbc53fe24c9d54a6148c5236274de72a8ea
kamranrasul <kamran.rasul@gmail.com>
2020-07-26 23:06:39 -0600

    sedning correction

--- src/main.cpp
+++ src/main.cpp
@@ -81,32 +81,6 @@ void wireSetup()
 // setting up esp NOW
 void espNowSetup()
 {
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
-
-  // Init ESP-NOW
-  if (esp_now_init() != ESP_OK)
-  {
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
-
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
-  esp_now_register_send_cb(OnDataSent);
-
-  // Register peer
-  esp_now_peer_info_t peerInfo;
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;
-  peerInfo.encrypt = false;
-
-  // Add peer
-  if (esp_now_add_peer(&peerInfo) != ESP_OK)
-  {
-    Serial.println("Failed to add peer");
-    return;
-  }
 }
 
 // reading Pin Status
@@ -141,7 +115,8 @@ void espNowSend()
 }
 
 // reading from MPU
-void mpu_read() {
+void mpu_read()
+{
   // setting the controller for read
   Wire.beginTransmission(MPU_addr);
 
@@ -168,7 +143,33 @@ void setup()
 
   pinSetup();
   wireSetup();
-  espNowSetup();
+  
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
+    Serial.println("Failed to add peer");
+    return;
+  }
 }
 
 void loop()
