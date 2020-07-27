f6c85d2bf1d6bf3116435d80d4717d65800a01ec
kamranrasul <kamran.rasul@gmail.com>
2020-07-26 21:23:59 -0600

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,16 @@
 #include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
+#include <Wire.h>
+
+// temperature register of MPU-6050
+#define TEMP_REG 0X41
+
+// I2C address of the MPU-6050
+const int MPU_addr = 0x68;
+
+// variable for storing the register value
+int16_t reading;
 
 // defining the pins need to control and monitor
 #define INPIN_1 0
@@ -25,6 +35,9 @@ typedef struct struct_message
 {
   int id; // must be unique for each sender board
   uint8_t pinStatus[8];
+  float temperature;
+  float humidity;
+  float pressure;
 } struct_message;
 
 //Create a struct_message called myData
@@ -37,14 +50,9 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-void setup()
+// setting up pin in input mode
+void pinSetup()
 {
-  // Init Serial Monitor
-  Serial.begin(115200);
-
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
-
   // Setting pin for read mode
   pinMode(INPIN_1, INPUT_PULLUP);
   pinMode(INPIN_2, INPUT_PULLUP);
@@ -54,6 +62,27 @@ void setup()
   pinMode(INPIN_6, INPUT_PULLUP);
   pinMode(INPIN_7, INPUT_PULLUP);
   pinMode(INPIN_8, INPUT_PULLUP);
+}
+
+// setting up Wire Transfer !2C
+void wireSetup()
+{
+  // I2C setup
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+
+  // PWR_MGMT_1 register
+  Wire.write(0x6B);
+  // set to zero (wakes up the MPU-6050)
+  Wire.write(0);
+  Wire.endTransmission(true);
+}
+
+// setting up esp NOW
+void espNowSetup()
+{
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
 
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK)
@@ -80,7 +109,8 @@ void setup()
   }
 }
 
-void loop()
+// reading Pin Status
+void readPins()
 {
   // Set values to send
   myData.id = 1;
@@ -92,7 +122,11 @@ void loop()
   myData.pinStatus[5] = digitalRead(INPIN_6);
   myData.pinStatus[6] = digitalRead(INPIN_7);
   myData.pinStatus[7] = digitalRead(INPIN_8);
+}
 
+// esp NOW send message
+void espNowSend()
+{
   // Send message via ESP-NOW
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
 
@@ -104,5 +138,49 @@ void loop()
   {
     Serial.println("Error sending the data");
   }
+}
+
+// reading from MPU
+void mpu_read() {
+  // setting the controller for read
+  Wire.beginTransmission(MPU_addr);
+
+  // setting for register for reading the value
+  Wire.write(TEMP_REG);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 2, true);
+
+  // reading 2 registers 0x41 and 0x42, as high and low
+  reading = Wire.read() << 8 | Wire.read();
+
+  // converting to from raw to engineering value
+  myData.temperature = reading / 340.0 + 36.53;
+
+  // displaying on the serial output
+  Serial.printf("Temperature: %.2f °C or %.2f °F", myData.temperature, (myData.temperature * 1.8) + 32);
+  Serial.println();
+}
+
+void setup()
+{
+  // Init Serial Monitor
+  Serial.begin(115200);
+
+  pinSetup();
+  wireSetup();
+  espNowSetup();
+}
+
+void loop()
+{
+  // reading Pins
+  readPins();
+
+  // reading from MPU
+  mpu_read();
+
+  // sending message
+  espNowSend();
+
   delay(10000);
 }
